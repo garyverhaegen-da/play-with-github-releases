4e723558f4ec0e50ad25baa276616e7a111c8039
Gary Verhaegen <gary.verhaegen@digitalasset.com>
2019-04-12 18:06:04 +0100

    azure

--- release.sh
+++ release.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 
 if [ "$(git show HEAD:VERSION)" != "$(git show HEAD~1:VERSION)" ] && [ "VERSION" = "$(git diff-tree --no-commit-id --name-only HEAD HEAD~1)" ]; then
     echo "make a release"
+    echo '##vso[task.setvariable variable=has_released]true'
     echo '##vso[task.setvariable variable=has_released;isOutput=true]true'
     echo "##vso[task.setvariable variable=release_tag]$(cat VERSION)"
 else
